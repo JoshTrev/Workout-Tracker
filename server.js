6b67940610cfdb6f8ab9e988dcfddd7a016506b8
JoshTrev <joshj438t@gmail.com>
2020-10-20 16:53:49 -0400

    Set up a Routes folder to handle routing

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const logger = require("morgan");
 
 const app = express();
+
+app.use(logger("dev"));
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,8 +13,23 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", { u
 
 app.use(express.static("public"));
 
+// API ROUTES
+
+// POST /api/workouts
+// PUT /api/workouts:id
+// GET /api/workouts/range (Last 7 days: Think LIMIT(7))
+
+// HTML ROUTES
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
+})
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/exercise.html"));
+})
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/stats.html"));
 })
 
 app.listen(PORT, () => {
